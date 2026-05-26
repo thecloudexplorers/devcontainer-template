1ff978da528770dc3f910321539e2ead1fbaeac5
Wesley Camargo <camargo.wes@gmail.com>
2026-05-27 00:44:56 +0200

    update devcontainer structure with dockerfile

--- .devcontainer/install.ps1
+++ .devcontainer/install.ps1
@@ -1,7 +1,10 @@
 #----------------------------------------------------------------------
-# This script is used to setup custom user configurations in a devcontainer environment.
-# Update the script with your custom configurations and it will be executed during the devcontainer build process,
-# Or point it to an external script that contains your custom configurations.
-#----------------------------------
+# Post-create user configuration. All system dependencies are installed
+# in the Dockerfile; this script handles user-level terminal setup only.
+#----------------------------------------------------------------------
 
-iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/wesleycamargo/terminal-bootstrap/refs/heads/master/terminal-setup.ps1')) 
+try {
+  iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/wesleycamargo/terminal-bootstrap/refs/heads/master/terminal-setup.ps1'))
+} catch {
+  Write-Warning "terminal-bootstrap failed: $_"
+}
